c947bd7d736ae6e6267ef80200d465f23eeeffe4
Arsen <arsen.pidhoretskyi@gmail.com>
2021-05-11 23:57:38 +0300

    JWT tokens

--- SoftServe_BackEnd/SoftServe_BackEnd/ServiceExtensions.cs
+++ SoftServe_BackEnd/SoftServe_BackEnd/ServiceExtensions.cs
@@ -51,11 +51,6 @@ namespace SoftServe_BackEnd
             services.AddAuthorization();
         }
 
-        public static void ConfigureIdentity(this IServiceCollection services)
-        {
-            services.AddIdentity<Client, IdentityRole>().AddEntityFrameworkStores<DatabaseContext>();
-        }
-
         public static void ConfigureSwagger(this IServiceCollection services)
         {
             services.AddSwaggerGen(c =>
@@ -65,10 +60,37 @@ namespace SoftServe_BackEnd
                     Title = "SoftServe_BackEnd",
                     Version = "v1"
                 });
+
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 Console.WriteLine(xmlPath);
                 c.IncludeXmlComments(xmlPath);
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description =
+                        "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+
+                    }
+                });
             });
         }
     }
